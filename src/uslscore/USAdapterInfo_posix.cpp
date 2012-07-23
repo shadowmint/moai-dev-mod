67fd87c6b9ac0fd30d804d5c2d6b69a11cda6dd8
Doug <linderd@iinet.net.au>
2012-07-23 06:01:22 +0000

    Updated get macaddress to work in linux

--- src/uslscore/USAdapterInfo_posix.cpp
+++ src/uslscore/USAdapterInfo_posix.cpp
@@ -30,10 +30,11 @@
 #include <uslscore/USAdapterInfo.h>
 
 #if !( NACL || ANDROID )
-#include <sys/socket.h>
-#include <sys/sysctl.h>
-#include <net/if.h>
-#include <net/if_dl.h>
+#include <sys/ioctl.h>
+#include <net/if.h> 
+#include <unistd.h>
+#include <netinet/in.h>
+#include <string.h>
 #endif
 
 //================================================================//
@@ -55,34 +56,37 @@ STLString USAdapterInfo::GetMACAddress () {
 	memset ( macAddress.bytes , 0 , 6 );
     
 #if !( NACL || ANDROID )
-	int mgmtInfoBase[6];
-	mgmtInfoBase[0] = CTL_NET;
-	mgmtInfoBase[1] = AF_ROUTE;
-	mgmtInfoBase[2] = 0;              
-	mgmtInfoBase[3] = AF_LINK;
-	mgmtInfoBase[4] = NET_RT_IFLIST;
-    
-	if ( !(( mgmtInfoBase [ 5 ] = if_nametoindex ( "en0" )) == 0 ) ) {
-		
-		size_t length;
-		if ( !( sysctl ( mgmtInfoBase, 6, NULL, &length, NULL, 0 ) < 0 ) ) {
-			
-	    	if ( !(( msgBuffer = ( char * ) malloc ( length )) == NULL ) ) {
-		
-	    		if ( sysctl ( mgmtInfoBase, 6, msgBuffer, &length, NULL, 0 ) < 0 ) {
-					//error
-				}
-				
-				struct if_msghdr *interfaceMsgStruct = ( struct if_msghdr * ) msgBuffer;
+   struct ifreq ifr;
+    struct ifconf ifc;
+    char buf[1024];
+    int success = 0;
 
-				struct sockaddr_dl *socketStruct = ( struct sockaddr_dl * ) ( interfaceMsgStruct + 1 );
+    int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP);
+    if (sock == -1) { /* handle error*/ };
 
-				memcpy ( macAddress.bytes, socketStruct->sdl_data + socketStruct->sdl_nlen, 6 );
-				
-				free(msgBuffer);
-			}
-		}
-	}
+    ifc.ifc_len = sizeof(buf);
+    ifc.ifc_buf = buf;
+    if (ioctl(sock, SIOCGIFCONF, &ifc) == -1) { /* handle error */ }
+
+    struct ifreq* it = ifc.ifc_req;
+    const struct ifreq* const end = it + (ifc.ifc_len / sizeof(struct ifreq));
+
+    for (; it != end; ++it) {
+        strcpy(ifr.ifr_name, it->ifr_name);
+        if (ioctl(sock, SIOCGIFFLAGS, &ifr) == 0) {
+            if (! (ifr.ifr_flags & IFF_LOOPBACK)) { // don't count loopback
+                if (ioctl(sock, SIOCGIFHWADDR, &ifr) == 0) {
+                    success = 1;
+                    break;
+                }
+            }
+        }
+        else { /* handle error */ }
+    }
+
+    unsigned char mac_address[6];
+
+    if (success) memcpy(mac_address, ifr.ifr_hwaddr.sa_data, 6);
 #else
 	//ANDROID NOT IMPLEMENTED
 #endif
